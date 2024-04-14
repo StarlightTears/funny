ec63858fe2b688a26319bab3babebbc93976a8ec
Dinh Viet Tien <dinh.viet.tien@prudential.com.vn>
2024-04-14 14:22:55 +0700

    update code

--- js/myJs.js
+++ js/myJs.js
@@ -1,14 +1,14 @@
 const textConfig = {
-  text1: "Hế luu Đồ đáng iêu!",
+  text1: "Hế luu e cuối tuần vui vẻ!",
   text2: "Ngốc đồ có điều này muốn hỏi e nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Thứ 3 thu xếp thời gian, đi chơi với a hem ._.",
+  text3: "Tối Thứ 2 a qua chơi + bóp dzú em dc hem ._.",
   text4:
     "Nếu e ko trả lời mà thoát ra tức là đồng ý rùi đó nha :v, không oke thì cũng oke",
   text5: "Không thích đi nha :))",
   text6: "Đi thì đi không đi thì đi :))",
   text7: "Gửi em 1 vài trò linh tinh nè, nhập các thứ linh tinh đi :vvvv",
   text8: "Gửi thử di <3",
-  text9: "https://sillylibra.com",
+  text9: "=)) Khum có gì đâu. Ấn gửi luôn đi",
   text10: "Thế là nhận lời rồi nhé",
   text11: "Tối a qua chơi :DDDDDDDDDDDDD",
   text12: "Okii lunn <3, Nhấn vô đây coi nè =))",
@@ -137,7 +137,8 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://sillylibra.com/";
+            window.location =
+              "https://sillylibra.com/blogs/news/can-dam-de-song-hanh-phuc";
           },
         });
       }
